c64b46eef7bd37c6ff20dddadb492852f87df6c1
dharshakch97-cloud <dharshak.ch97@msitprogram.net>
2019-10-22 12:21:52 +0530

    LibraryCatalog Done

--- Module 4 Encapsulation/Library Card Catalog/CardCatalog.java
+++ Module 4 Encapsulation/Library Card Catalog/CardCatalog.java
@@ -1,58 +1,62 @@
 class CardCatalog {
-    Card[]cardArray;
-    int index = 0;
+    Card[] cardArray;
+    int count;
 
     CardCatalog() {
-        cardArray = new Card[10];
+        this.count = 0;
+        this.cardArray = new Card[100];
     }
 
-    public void AddCard(Card obj) {
-        cardArray[index] = obj;
-        index += 1;
+    public void AddCard(Card card) {
+        cardArray[count] = card;
+        count++;
     }
-    public Card GetATitle(String s1) {
-        for(int i = 0; i<cardArray.length; i++) {
-            if(cardArray[i].title.equals(s1)) {
+
+    public Card searchTitle(String searchTitle) {
+        for (int i = 0; i < count; i++) {
+            if (cardArray[i].getTitle() == searchTitle) {
                 return cardArray[i];
             }
         }
         return null;
     }
-    public Card[] GetAuthor(String s2) {
-        Card[] arr = new Card[index];
-        int count = 0;
-        for(int i = 0; i<cardArray.length; i++) {
-            if(cardArray[i].author.equals(s2)) {
-                arr[count] = cardArray[i];
-                count++;
+
+    public Card searchAuthor(String searchAuthor) {
+        for (int i = 0; i < count; i++) {
+            if (cardArray[i].getAuthor() == searchAuthor) {
+                return cardArray[i];
             }
         }
-        return arr;
+        return null;
     }
-            
-    public Card[] getSubject(String s3) {
-        Card[] arr = new Card[index];
-        int count = 0;
-        for(int i = 0; i<cardArray.length; i++) {
-            if(cardArray[i].subject.equals(s3)) {
-                arr[count] = cardArray[i];
-                count++;
+
+    public Card searchSubject(String searchSubject) {
+        for (int i = 0; i < count; i++) {
+            if (cardArray[i].getSubject() == searchSubject) {
+                return cardArray[i];
             }
         }
-        return arr;
+        return null;
     }
-    public void delete(String s4) {
-        for(int i = 0; i<cardArray.length; i++) {
-            if(cardArray[i].title.equals(s4)) {
-                cardArray[i] = null;
+        
+    public boolean removeCard(String removeTitle) {
+        for (int i = 0; i < count; i++) {
+            if (cardArray[i].getTitle() == removeTitle) {
+                for (int j = i + 1; j < count; j++) {
+                    cardArray[i] = cardArray[j];
+                    i++;
+                }
+                cardArray[count] = null;
+                count--;
+                return true;
             }
-        } 
+        }
+        return false;
     }
-    public void print_Catalog() {
-        for(int i = 0; i<cardArray.length; i++) {
-            if(this.cardArray[i] != null) {
-                System.out.println(cardArray[i]);
-            }
+    
+    public void printCatalog() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(cardArray[i]);
         }
     }
 }
--- Module 4 Encapsulation/Library Card Catalog/Menu.java
+++ Module 4 Encapsulation/Library Card Catalog/Menu.java
@@ -3,15 +3,21 @@ class Menu {
         Card card1 = new Card("harry", "Potter", "thriller");
         Card card2 = new Card("histopan", "aditya bravo", "action");
         Card card3 = new Card("half girl friend", "chetal bhagat", "romance");
+
         CardCatalog cardcatalog = new CardCatalog();
+        
         cardcatalog.AddCard(card1);
         cardcatalog.AddCard(card2);
         cardcatalog.AddCard(card3);
-        cardcatalog.print_Catalog();
-        Card get_title = cardcatalog.GetATitle("harry");
-        System.out.println(get_title);
-        // cardcatalog.delete("harry");
-        // Card[] get_subject = cardcatalog.getSubject("action");
-        // cardcatalog.print_Catalog();
+        
+        cardcatalog.printCatalog();
+        System.out.println();
+        Card getTitle = cardcatalog.searchTitle("harry");
+        System.out.println(getTitle);
+        cardcatalog.removeCard("harry");
+    
+        Card getSubject = cardcatalog.searchSubject("action");
+        System.out.println();    
+        cardcatalog.printCatalog();
     }
 }
